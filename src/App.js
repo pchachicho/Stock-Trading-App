61243c0869c48a08ca94856c64fe5e7d81df446c
Patrick Hachicho <hachichopatrick@gmail.com>
2023-02-18 21:44:31 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        {/* <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/contact" component={Contact} /> */}
+      </Routes>
+    </Router>
+    
+     
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+// import { Button } from "./Button";
+// import food from './photo/Plate.png'
+
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click); // clicking changes the icon
+  const closeMobileMenu = () => setClick(false); // When clicking on links it closes navbar
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener("resize", showButton);
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <span style={{ fontWeight: 'bold' }}>Stocks</span>
+          {/* <img src={food} className="food" alt="food" /> */}
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/National" className="nav-links" onClick={closeMobileMenu}>
+              National
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/Countries"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Countries
+            </Link>
+          </li>
+        </ul>
+       {/* {button && <Button buttonStyle="btn--outline">About</Button>} */}
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
+// Home, About, Countries
